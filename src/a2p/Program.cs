d2f09e7f5bbe34d93ee985c1c49082cf87d5dcac
David Whatley <nsxdavid@gmail.com>
2025-02-23 10:47:13 -0600

    Add initial CLI project with hello world

---
+++ src/a2p/Program.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 David Whatley
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+
+namespace a2p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
