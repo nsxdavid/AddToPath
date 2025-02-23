24a9fcb0d0955eeeb7e895bb684fd992b9dbecca
David Whatley <nsxdavid@gmail.com>
2025-02-23 03:34:51 -0600

    Add MIT license headers to source files
    - Add copyright and license headers to MainForm.cs and PathsDialog.cs
    - Remove .editorconfig as it's not needed
    - Update copyright holder name in LICENSE file

--- PathsDialog.cs
+++ PathsDialog.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 David Whatley
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System;
 using System.Drawing;
 using System.Linq;
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 David Whatley
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System;
 using System.Windows.Forms;
 using Microsoft.Win32;
